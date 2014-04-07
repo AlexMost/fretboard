275735255e3066176113f8b870cf5d459345b703
AlexMost <alexmost1989@gmail.com>
2014-04-07 23:40:35 +0300

    Improved script for generating sounds from wav

--- thirdparty/scripts.sh
+++ thirdparty/scripts.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
 # inliner for converting wav files to mp3
-for file in *.wav; do lame -b 192 -h "$file" "${file%.wav}.mp3"; done && mkdir mp3 && mv *.mp3 mp3 && mkdir wav && mv *.wav wav
+for i in {1..6}
+do
+    cd resources/${i}string/wav
+    
+    for file in *.wav
+    do
+        lame -b 192 -h "$file" "${file%.wav}.mp3"
+    done
+
+    mkdir -p ../mp3 && mv *.mp3 ../mp3
+done
