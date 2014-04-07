b846d1474be2debd23edabda4eaaaaa43fcc0e86
AlexMost <alexmost1989@gmail.com>
2014-04-08 00:19:46 +0300

    convering 0th wav files to mp3

--- thirdparty/scripts.sh
+++ thirdparty/scripts.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 
 # inliner for converting wav files to mp3
-for i in {1..6}
-do
-    cd resources/${i}string/wav
+cd resources/$1string/wav
 
-    for file in *.wav
-    do
-        lame -b 192 -h "$file" "${file%.wav}.mp3"
-    done
-
-    mkdir -p ../mp3 && mv *.mp3 ../mp3
+for file in *.wav
+    do lame -b 192 -h "$file" "${file%.wav}.mp3"
 done
+
+mkdir -p ../mp3 && mv *.mp3 ../mp3
